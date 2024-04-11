0dcdc60bd0fa12da734aa3c982fbb2d69825c58f
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-04-11 15:18:37 +0330

    adding ref to input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
 
 function App() {
+  const userNameInput = useRef<HTMLInputElement>(null);
+  console.log(userNameInput);
   return (
     <main className="bg-gray-600 w-[60%] mx-auto mt-10 p-6 rounded-md flex flex-col items-center gap-2">
-      <Input label="name" id="name" type="text" />
+      <Input label="name" id="name" type="text" ref={userNameInput} />
       <Input label="age" id="age" type="number" />
       <Button el="button">Botton</Button>
       <Button el="anchor" href="https://www.google.com/">
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,22 +1,25 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
 
 type InputProps = {
   id: string;
   label: string;
 } & ComponentPropsWithoutRef<"input">;
-const Input = ({ id, label, ...props }: InputProps) => {
-  return (
-    <p className="flex flex-row justify-between gap-2 text-white bg-blue-950 w-[90%] mx-auto p-6 rounded-md">
-      <label htmlFor={id} className="">
-        {label}
-      </label>
-      <input
-        id={id}
-        {...props}
-        className="w-full rounded-sm p-2 bg-yellow-100"
-      />
-    </p>
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, label, ...props }, ref) => {
+    return (
+      <p className="flex flex-row justify-between gap-2 text-white bg-blue-950 w-[90%] mx-auto p-6 rounded-md">
+        <label htmlFor={id} className="">
+          {label}
+        </label>
+        <input
+          id={id}
+          {...props}
+          ref={ref}
+          className="w-full rounded-sm p-2 bg-yellow-100"
+        />
+      </p>
+    );
+  }
+);
 
 export default Input;
