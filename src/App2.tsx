e3cd85bda9cc9e6fa41297657dd6239d44c3478b
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-04-11 19:01:02 +0330

    adding reset form functionality

--- src/App2.tsx
+++ src/App2.tsx
@@ -1,15 +1,25 @@
+import { useRef } from "react";
 import Button from "./components/Button";
-import Form from "./components/Form";
+import Form, { FormHandle } from "./components/Form";
 import Input from "./components/Input";
 
 const App2 = () => {
+  const customForm = useRef<FormHandle>(null);
   const saveHandler = (data: unknown) => {
     const extractedData = data as { name: string; age: number };
+    // if (
+    //     !data ||
+    //     typeof data !== 'object' ||
+    //     !('name' in data) ||
+    //     !('age' in data)
+    //   ) {
+    //       return;
     console.log(extractedData);
+    customForm.current?.clear();
   };
   return (
     <main className="bg-gray-600 w-[60%] mx-auto mt-10 p-6 rounded-md flex flex-col items-center gap-2">
-      <Form onSave={saveHandler}>
+      <Form onSave={saveHandler} ref={customForm}>
         <Input label="name" id="name" type="text" />
         <Input label="age" id="age" type="number" />
         <Button el="button">Submit</Button>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,45 @@
-import { ComponentPropsWithoutRef, FormEvent } from "react";
+import {
+  ComponentPropsWithoutRef,
+  FormEvent,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from "react";
+
+export type FormHandle = {
+  clear: () => void;
+};
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (value: unknown) => void;
 };
-const Form = ({ onSave, children, ...otherProps }: FormProps) => {
-  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const data = Object.fromEntries(formData);
-    onSave(data);
-  };
-  return (
-    <form
-      onSubmit={submitHandler}
-      {...otherProps}
-      className="w-full flex flex-col gap-2"
-    >
-      {children}
-    </form>
-  );
-};
+const Form = forwardRef(
+  ({ onSave, children, ...otherProps }: FormProps, ref) => {
+    const form = useRef<HTMLFormElement>(null);
+    useImperativeHandle(ref, () => {
+      return {
+        clear() {
+          form.current?.reset();
+        },
+      };
+    });
+    const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const formData = new FormData(event.currentTarget);
+      const data = Object.fromEntries(formData);
+      onSave(data);
+    };
+    return (
+      <form
+        onSubmit={submitHandler}
+        {...otherProps}
+        ref={form}
+        className="w-full flex flex-col gap-2"
+      >
+        {children}
+      </form>
+    );
+  }
+);
 
 export default Form;
